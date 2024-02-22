04f346ae2194abae46360a46ed3bf27fb5268c91
Bhumi Joshi <bhumij.tagline@gmail.com>
2024-02-22 12:56:18 +0530

    Question no.8

---
+++ que_8.py
@@ -0,0 +1,9 @@
+import itertools
+numbers = [2, 5, 6, 1, 3, 8, 9, 10]
+
+for i in range(0,len(numbers)-1):
+    for j in range(i+1,len(numbers)-1):
+        if numbers[i] > numbers[j]:
+            numbers[i],numbers[j] = numbers[j],numbers[i]
+
+print(numbers)
